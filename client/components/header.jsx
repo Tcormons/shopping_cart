b8b6bd3d5f92cb9658ae0713e6ccc8899fe6cc62
tcormons <tcormons@gmail.com>
2019-11-21 19:20:47 +0000

    user can see cart summary with total and return to catalog | Tomas Cormons

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -13,7 +13,9 @@ class Header extends React.Component {
           <div className="fa fa-dollar-sign m-1 fa-2x icon"></div>
           <h2 className="ml-1">Wicked Sales</h2>
         </div>
-        <div className="d-flex">
+        <div className="d-flex"
+          onClick={this.props.checkout}
+          style={{ cursor: 'pointer' }}>
           <h6 className="mt-3">{this.props.itemCount} Items</h6>
           <div className="fa fa-shopping-cart ml-2 mt-3 fa-lg icon"></div>
         </div>
