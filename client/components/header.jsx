8c390909765a4f673b153d31bd4c57541053fc67
tcormons <tcormons@gmail.com>
2019-12-19 17:29:07 +0000

    updated styling for mobile header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -10,15 +10,13 @@ class Header extends React.Component {
     return (
       <div>
         <div className="header col-sm-12 sticky-top bg-dark text-white shadow d-flex justify-content-between">
-          <div className="d-flex display-5 align-self-center">
-            <div className="icon align-self-center"></div>
-            <h1 className="banner-title ml-2 mt-1">Local Sports</h1>
-          </div>
+          <div className="icon align-self-center"></div>
+          <h1 className="banner-title align-self-center">Local Sports</h1>
           <div className="d-flex align-self-center"
             onClick={this.props.checkout}
             style={{ cursor: 'pointer' }}>
-            <h6 className="mt-3">{this.props.itemCount} Items</h6>
-            <div className="fa fa-shopping-cart ml-2 mt-3 fa-lg"></div>
+            <h6 className="align-self-center">{this.props.itemCount} Items</h6>
+            <div className="fa fa-shopping-cart fa-lg align-self-center"></div>
           </div>
         </div>
       </div>
